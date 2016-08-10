b61200129f925749ef177a7b280002e45b4359a9
KaptenNeptun <kaptenneptun@gmail.com>
2016-08-10 19:57:34 +0200

    Added framework for Turnhandling and Actor class

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Actor
+{
+    float delay=0;
+
+    public virtual void Act() {
+        
+    }
+}  
+
+public class Effect : Actor
+{
+    public override void Act()
+    {
+        
+    }
+}
+public class Character : Actor
+{
+
+    public override void Act()
+    {
+        base.Act();
+    }
+}
---
+++ Assets/Scripts/TurnHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TurnHandler : MonoBehaviour {
+
+    List<Actor> actors;
+
+    public int factionTurn;
+    public float timeCounter = 0;
+    
+    // Use this for initialization
+    void Start () {
+        factionTurn = 0;
+        actors = new List<Actor>();
+
+        actors[0].Act();
+    }
+        
+	
+	
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
