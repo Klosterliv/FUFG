857b8cd3c222707b871c6291caa2f7de38e1e844
KaptenNeptun <kaptenneptun@gmail.com>
2016-08-10 21:45:53 +0200

    Split up Actor into several childs

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Unit : Actor
+{
+    public int moveSpeed;
+    public int minorActions;
+    public int majorActions;
+    public override void Act()
+    {
+        //base.Act();
+    }
+}
